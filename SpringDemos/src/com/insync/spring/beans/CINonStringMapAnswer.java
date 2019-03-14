71bd6077f79c509d2bad3fd22f2382c47402b9c4
Sasi <kondapallishashi@gmail.com>
2019-03-14 20:52:53 +0530

    Constructor Injection Map Collection using Non String Values

--- SpringDemos/src/com/insync/spring/beans/CINonStringMapAnswer.java
+++ SpringDemos/src/com/insync/spring/beans/CINonStringMapAnswer.java
@@ -1,5 +1,24 @@
 package com.insync.spring.beans;
 
-public class CINonStringMapAnswer {
+import java.util.Date;
 
+public class CINonStringMapAnswer {
+	private int id;
+	private String answer;
+	private Date postedDate;
+	
+	public CINonStringMapAnswer() {
+		//Default Constructor
+	}
+	
+	public CINonStringMapAnswer(int id, String answer, Date postedDate) {
+		this.id=id;
+		this.answer=answer;
+		this.postedDate=postedDate;
+	}
+	
+	public String toString() {
+	
+		return "ID: "+id+" Answer: "+answer+" Posted Date: "+postedDate;
+	}
 }
--- SpringDemos/src/com/insync/spring/beans/CINonStringMapUser.java
+++ SpringDemos/src/com/insync/spring/beans/CINonStringMapUser.java
@@ -1,5 +1,20 @@
 package com.insync.spring.beans;
 
 public class CINonStringMapUser {
+	private int id;
+	private String name, email;
+	
+	public CINonStringMapUser() {
+		//Default Constructor
+	}
+	
+	public CINonStringMapUser(int id, String name, String email) {
+		this.id=id;
+		this.name=name;
+		this.email=email;
+	}
 
+	public String toString() {
+		return "ID: "+id+" Name: "+name+" Email: "+email;
+	}
 }
